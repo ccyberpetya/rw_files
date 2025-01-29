3cfe22920cec500604d23aaf87b36127839c113f
ccyberpetya <petya.yudin2@gmail.com>
2025-01-29 17:35:59 +0500

    New_Commit

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-with open('recipes.txt',  encoding='utf-8') as f:
-    cook_book = {}
-    for i in f:
-        name = i.strip()
-        name_count = f.readline()
-        ingredients = []
-        for ing in range(int(name_count)):
-            i1,i2,i3 = f.readline().strip().split('|')
-            ingredients.append({'ingredient_name':i1, 'quantity':i2,'measure':i3})
-        f.readline()
-        cook_book[name] = ingredients
-#print(cook_book)
+import os
+def read_cook_book():
+    with open('recipes.txt',  encoding='utf-8') as f:
+        cook_book = {}
+        for i in f:
+            name = i.strip()
+            name_count = f.readline()
+            ingredients = []
+            for ing in range(int(name_count)):
+                i1,i2,i3 = f.readline().strip().split('|')
+                ingredients.append({'ingredient_name':i1, 'quantity':i2,'measure':i3})
+            f.readline()
+            cook_book[name] = ingredients
+    return cook_book
 
 
-def get_shop_list_by_dishes(dishes:list,person_count:int):
+def get_shop_list_by_dishes(cook_book,dishes:list,person_count:int):
     shop_list = {}
     for dish in dishes:
         if dish in cook_book:
@@ -23,29 +25,31 @@ def get_shop_list_by_dishes(dishes:list,person_count:int):
                     shop_list[i['ingredient_name']] = {'measure':i['measure'],'quantity': int(i['quantity'])* person_count}
         else:
             print('Данного блюда нет:')
-    #print(shop_list)
-get_shop_list_by_dishes(['Утка по-пекински', 'Запеченный картофель'], 4)
+    print(shop_list)
+cook_book = read_cook_book()
+get_shop_list_by_dishes(cook_book,['Фахитос', 'Запеченный картофель'], 4)
             
 
+def process_files(directory, output_file):
+    file_data = {}
+    
+    for file_name in os.listdir(directory):
+        file_path = os.path.join(directory, file_name)
+        if os.path.isfile(file_path):
+            with open(file_path, encoding='utf-8') as file:
+                line_length = sum(1 for _ in file)
+                file_data[line_length] = [file_name, line_length]
+    
+    sorted_data = dict(sorted(file_data.items()))
+    
+    with open(output_file, 'a', encoding='utf-8') as result_file:
+        for value in sorted_data.values():
+            result_file.write(f'{value[0]}\n')
+            result_file.write(f'Строка номер {value[1]} файла номер {value[0]}\n')
+    
+    with open(output_file, encoding='utf-8') as result_file:
+        print(result_file.read())
 
-new_dict = {}
-with open('1.txt', encoding='utf-8') as file_1:
-    file_name1 = '1.txt'
-    count_name1 = len(file_1.readline())
-    with open('2.txt', encoding='utf-8') as file_2:
-        file_name2 = '2.txt'
-        count_name2 = len(file_2.readline())
-        with open('3.txt', encoding='utf-8') as file_3:
-            file_name3 = '3.txt'
-            count_name3 = len(file_3.readline())
-            new_dict = {count_name1:[file_name1,count_name1],count_name2:[file_name2,count_name2],count_name3:[file_name3,count_name3]}
-sort_dict = dict(sorted(new_dict.items()))
-with open('result.txt', 'a', encoding='utf-8') as file_final:
-    for value in sort_dict.values():
-        for i in value:
-            file_final.write(f'{i}\n')
-            file_final.write(f'Строка номер {value[1]} файла номер {value[0]}\n')
-with open('result.txt',encoding='utf-8') as file_result:
-    print(file_result.read())
-        
-        
+directory = 'C:/Users/CUCUBERPETYA/Desktop/rw_files'  
+output_file = 'result.txt'
+process_files(directory, output_file)
